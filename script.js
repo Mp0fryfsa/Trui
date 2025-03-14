2fb6abefac1aae5a96afec8434c892f9759a585e
aeer <88292734+Mp0fryfsa@users.noreply.github.com>
2025-03-14 21:46:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-function startchat() {
-    window.open('https://smrturl.co/d61cde5',);
-}
+<script type="text/javascript">
+    var RlVHL_WTu_XilsZc={"it":2156818,"key":"aa46c"};
+</script>
+<script src="https://d29gqhzevia104.cloudfront.net/b8eed66.js"></script>
